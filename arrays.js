ff0e7be6a5d2e040794f7c8edd5ae80278b5bf7f
Pablo Cuadros <pablocuadrosz115@gmail.com>
2018-01-14 12:40:23 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittle"]
+
+function addElementToBeginningOfArray(array,element) {
+  var newarray = [element, ...array]
+  return newarray
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array = [element, ...array]
+  return array
+}
+
+function addElementToEndOfArray(array,element) {
+    var newarray = [...array, element]
+  return newarray
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array = [...array, element]
+  return array
+}
+
+function accessElementInArray(array,index) {
+  return array[index]
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+
