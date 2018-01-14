d81cbf18b1125598fbd96ded307330b0270ab996
Pablo Cuadros <pablocuadrosz115@gmail.com>
2018-01-14 12:44:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice
+  return array.slice()
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
