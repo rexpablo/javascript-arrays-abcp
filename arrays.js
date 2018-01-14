0816ae46052622aad0f9c461bddbef814bf3d784
Pablo Cuadros <pablocuadrosz115@gmail.com>
2018-01-14 12:52:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function addElementToEndOfArray(array,element) {
 }
 
 function destructivelyAddElementToEndOfArray(array,element) {
-  array = [...array, element]
+ array.push(element)
   return array
 }
 
